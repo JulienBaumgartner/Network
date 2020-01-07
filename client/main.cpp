8074d380eb7f08240a663750a0db1a4174441c86
JulienBaumgartner <julien.baumgartner04@gmail.com>
2020-01-07 17:03:47 +0100

    chat

--- client/main.cpp
+++ client/main.cpp
@@ -1,4 +1,136 @@
-int main(int argc, char* argv[])
+#if defined(_WIN32) || defined(_WIN64)
+#include <WinSock2.h>
+#else
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+#include <stdio.h>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <list>
+#include <ws2tcpip.h>
+#include <conio.h>
+#if defined(_WIN32) || defined(_WIN64)
+#pragma comment(lib, "ws2_32.lib")
+#else
+#define SOCKET int
+#define INVALID_SOCKET (-1)
+#define SOCKET_ERROR (-1)
+#endif
+
+bool select_recv(SOCKET sock, int interval_us = 1)
 {
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(sock, &fds);
+	timeval tv;
+	tv.tv_sec = 0;
+	tv.tv_usec = interval_us;
+	return (select(sock + 1, &fds, 0, 0, &tv) == 1);
+}
+
+int main(int ac, char** av) {
+	if (ac != 3)
+	{
+		std::cerr << "error incorrect argument list:" << std::endl;
+		std::cerr << "program_name server port" << std::endl;
+		return -1;
+	}
+#if defined(_WIN32) || defined(WIN64)
+	WSADATA ws_data;
+	if (WSAStartup(MAKEWORD(2, 2), &ws_data) != 0)
+	{
+		std::cerr << "cannot initialize winsock!" << std::endl;
+		return -1;
+	}
+#endif
+	SOCKET client = INVALID_SOCKET;
+	if ((client = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		std::cerr << "could not create socket!" << std::endl;
+		return -1;
+	}
+	sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(std::atoi(av[2]));
+#if defined(_WIN32) || defined (_WIN64)
+	server_addr.sin_addr.S_un.S_addr = inet_addr(av[1]);
+#else
+	if (inet_pton(AF_INET, av[1], &server_addr.sin_addr))
+	{
+		std::cerr << "invalid address " << av[1] << std::endl;
+		return -1;
+	}
+#endif
+	if (connect(client, (sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+	{
+		std::cerr << "connection error" << std::endl;
+		return -1;
+	}
+	std::cout << "Connected to " << av[1] << ":" << av[2] << std::endl;
+	std::cout << "\tdisconnect by typing /quit" << std::endl;
+	std::cout << "Enter your name:";
+
+	bool nameSent = false;
+	std::string name;
+	while (!nameSent) {
+		std::cin >> name;
+		if (name.size() != 0)
+		{
+			send(client, name.data(), name.size(), 0);
+			nameSent = true;
+		}
+		else 
+		{
+			std::cout << "Your name can't be empty!" << std::endl;
+		}
+	}
+	std::string input;
+	bool quit = false;
+	while (!quit)
+	{
+		if (_kbhit()) 
+		{
+			char c = _getch();
+			if (c == 13)
+			{
+				std::cout << "\r\n";
+				if (input.find("/quit") != std::string::npos)
+				{
+					quit = true;
+					continue;
+				}
+				if (input.size() != 0)
+				{
+					send(client, input.data(), input.size(), 0);
+				}
+				input.clear();
+			}
+			else 
+			{
+				std::cout << c;
+				input.push_back(c);
+			}
+		}
+		
+		if (select_recv(client)) {
+			std::string recv_string;
+			recv_string.resize(512);
+			int ret = recv(client, recv_string.data(), 512, 0);
+			recv_string.resize(ret); 
+			std::cout << recv_string << std::endl;
+		}
+	}
+#if defined(_WIN32) || defined(_WIN64)
+	closesocket(client);
+	WSACleanup();
+#else
+	close(client);
+#endif
 	return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,225 @@
 #include <WinSock2.h>
+#include <iostream>
+#include <list>
+#include <vector>
+#include <map>
 
 #pragma comment(lib, "ws2_32.lib")
 
+struct Player {
+	std::string Name;
+	SOCKET Socket;
+};
+
+std::list<Player> players;
+
+bool select_recv(SOCKET sock, int interval_us = 1)
+{
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(sock, &fds);
+	timeval tv;
+	tv.tv_sec = 0;
+	tv.tv_usec = interval_us;
+	return (select(sock + 1, &fds, 0, 0, &tv) == 1);
+}
+
+bool check_accept(SOCKET listening, SOCKET* client_ptr)
+{
+	SOCKET client = INVALID_SOCKET;
+	if (select_recv(listening))
+	{
+		client = accept(listening, nullptr, nullptr);
+	}
+	*client_ptr = client;
+	return (client != INVALID_SOCKET);
+}
+
+bool check_recv(SOCKET client, std::vector<char>& buffer)
+{
+	int result = 0;
+	if (select_recv(client))
+	{
+		const int MAX_SIZE = 512;
+		buffer.resize(MAX_SIZE, 0);
+		result = recv(client, &buffer[0], MAX_SIZE, 0);
+		if (result < 0) 
+		{
+			return false;
+		}
+		buffer.resize(result);
+		std::cout << "Receive " << result << "elements.\n";
+		if (result == 0)
+		{
+			result = -1;
+		}
+	}
+	return (result >= 0);
+}
+
+bool check_send(Player client, const std::map<SOCKET, std::vector<char>> &buffers)
+{
+	int result = 0;
+
+	for (const auto&[user, buffer] : buffers) 
+	{
+		if (buffer.size() > 0 && user != client.Socket)
+		{
+			result = send(client.Socket, &buffer[0], buffer.size(), 0);
+			std::cout << "Send " << buffer.size() << " elements.\n";
+		}
+	}
+
+	
+	return (result >= 0);
+}
+
+bool send_to_player(Player player, const std::string message)
+{
+	int result = 0;
+
+	std::vector<char> m(message.begin(), message.end());
+	result = send(player.Socket, &m[0], m.size(), 0);
+	std::cout << "Send " << m.size() << " elements.\n";
+
+	return (result >= 0);
+}
+
+bool send_to_all(const std::string message)
+{
+	int result = 0;
+
+	std::vector<char> m(message.begin(), message.end());
+	for (auto player : players)
+	{
+		result = send(player.Socket, &m[0], m.size(), 0);
+		std::cout << "Send " << m.size() << " elements.\n";
+	}
+
+	return (result >= 0);
+}
+
 int main(int argc, char* argv[])
 {
+	WSADATA ws_data;
+	if (WSAStartup(MAKEWORD(2, 0), &ws_data) != 0)
+	{
+		std::cerr << "cannot initialize winsock!\n";
+		return -1;
+	}
+
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		std::cerr << "Can't create socket!\n";
+		return -2;
+	}
+
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(42000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR)
+	{
+		std::cerr << "Could not bind to address.\n";
+		return -3;
+	}
+
+	if (listen(listening, SOMAXCONN) == SOCKET_ERROR)
+	{
+		std::cerr << "Could not listen to socket.\n";
+		return -4;
+	}
+	std::cout << "Wait for connection...\n";
+
+	std::list<SOCKET> sockets;
+	while(true)
+	{
+		SOCKET new_client;
+		if (check_accept(listening, &new_client))
+		{
+			sockets.push_back(new_client);
+			std::cout << "New client.\n";
+		}
+
+		for (auto client : sockets)
+		{
+			std::vector<char> buffer;
+			if (!check_recv(client, buffer))
+			{
+				sockets.remove_if([client](SOCKET sock)
+				{
+					return sock == client;
+				});
+				closesocket(client);
+				std::cout << "Client disconnected on recv.\n";
+				break;
+			}
+			if (buffer.size() != 0)
+			{
+				Player player;
+				player.Name = std::string(buffer.begin(), buffer.end());
+				player.Socket = client;
+				players.push_back(player);
+
+				send_to_all(player.Name + " join the room");
+
+				sockets.remove_if([client](SOCKET sock)
+				{
+					return sock == client;
+				});
+				break;
+			}
+		}
+
+		std::map<SOCKET, std::vector<char>> buffers;
+		for (auto player : players)
+		{
+			std::vector<char> buffer;
+			if (!check_recv(player.Socket, buffer))
+			{
+				send_to_all(player.Name + " leave the room");
+				players.remove_if([player](Player p)
+				{
+					return p.Socket == player.Socket;
+				});
+				closesocket(player.Socket);
+				std::cout << "Client disconnected on recv.\n";
+				break;
+			}
+			if (buffer.size() != 0)
+			{
+				std::vector<char> message(player.Name.begin(), player.Name.end());
+				message.push_back(':');
+				message.insert(message.end(), buffer.begin(), buffer.end());
+
+				buffers.insert({ player.Socket, message });
+			}
+		}
+
+		for (auto player : players)
+		{
+			if (buffers.size() != 0)
+			{
+				if (!check_send(player, buffers))
+				{
+					send_to_all(player.Name + " leave the room");
+					players.remove_if([player](Player p)
+					{
+						return p.Socket == player.Socket;
+					});
+					closesocket(player.Socket);
+					std::cout << "Client disconnected on send.\n";
+					break;
+				}
+			}
+		}
+		Sleep(1);
+	}
+
+	closesocket(listening);
+	WSACleanup();
+
 	return 0;
 }
